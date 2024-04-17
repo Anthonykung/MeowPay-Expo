bb40868be6ffdf9a71cdda91722b34df5f5d7ab6
Anthony Kung <hi@anth.dev>
2024-04-16 20:26:06 -0700

    update starter content

---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+/**
+* Copyright (c) 2024 Anthony Kung (anth.dev)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* @file   Header.tsx
+* @author Anthony Kung <hi@anth.dev> (anth.dev)
+* @date   Created on 04/16/2024 20:20:21
+*/
+
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function Header(props: any) {
+  const { top } = useSafeAreaInsets();
+  return (
+    <View style={{ paddingTop: top }}>
+      <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
+        <Link className="font-bold flex-1 items-center justify-center" href="/">
+          ACME
+        </Link>
+        <View className="flex flex-row gap-4 sm:gap-6">
+          <Link
+            className="text-md font-medium hover:underline web:underline-offset-4"
+            href="/"
+          >
+            About
+          </Link>
+          <Link
+            className="text-md font-medium hover:underline web:underline-offset-4"
+            href="/"
+          >
+            Product
+          </Link>
+          <Link
+            className="text-md font-medium hover:underline web:underline-offset-4"
+            href="/"
+          >
+            Pricing
+          </Link>
+        </View>
+      </View>
+    </View>
+  );
+}
